8b06c57c223dab0f0e9dd6e5f9289736c51c2a02
Davi Kawasaki <davishinjik@gmail.com>
2021-02-01 22:56:24 -0300

    fix: flags ignorePackages or null

--- bin/runner.js
+++ bin/runner.js
@@ -18,7 +18,7 @@ module.exports = (flags) => {
 		console.log(`semantic-release version: ${semrelPkgJson.version}`);
 		console.log(`flags: ${JSON.stringify(flags, null, 2)}`);
 
-		const ignorePackages = flags.ignorePackages ? flags.ignorePackages : [];
+		const ignorePackages = flags.ignorePackages ? flags.ignorePackages : null;
 
 		// Get list of package.json paths according to Yarn workspaces.
 		const paths = getWorkspacesYarn(cwd, ignorePackages);
